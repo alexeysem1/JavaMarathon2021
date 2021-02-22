aafbe7bc94ddf7f2f810e9ff0baace2b2abfd20c
Алексей <aleksej@MacBook-Pro-Aleksej-2.local>
2021-02-22 22:31:58 +0300

    add task1-6 solutions

--- src/main/java/day1/Task2.java
+++ src/main/java/day1/Task2.java
@@ -2,6 +2,9 @@ package day1;
 
 public class Task2 {
     public static void main(String[] args) {
-
+        int n=10;
+        for (int i=0;i<n;i++){
+            System.out.print("JAVA ");
+        }
     }
 }
