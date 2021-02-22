4523c08cc009481c21c3045fb9228e41091eaaf4
Алексей <aleksej@MacBook-Pro-Aleksej-2.local>
2021-02-22 22:17:47 +0300

    add tasks 1-6 solutions

--- src/main/java/day1/Task5.java
+++ src/main/java/day1/Task5.java
@@ -2,6 +2,9 @@ package day1;
 
 public class Task5 {
     public static void main(String[] args) {
-
+        int n=2020;
+        for (int i=1980;i<=2020;i+=4){
+            System.out.println("Олимпиада "+i+" года");
+        }
     }
 }
