efce900dfd847128ef4d84d4079aec10dff8e501
Алексей <aleksej@MacBook-Pro-Aleksej-2.local>
2021-02-23 19:35:24 +0300

    add day2 task1-4 solutions

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -1,7 +1,23 @@
 package day2;
 
+import java.util.Scanner;
+
+
 public class Task1 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int numberOfFloors = scan.nextInt();
+
+        if (numberOfFloors > 0 && numberOfFloors <= 4) {
+            System.out.println("Малоэтажный дом");
+        } else if (numberOfFloors >= 5 && numberOfFloors <= 8) {
+            System.out.println("Среднеэтажный дом");
+        } else if (numberOfFloors >= 9) {
+            System.out.println("Многоэтажный дом");
+        } else {
+            System.out.println("Ошибка ввода");
+        }
+
 
     }
 }
--- src/main/java/day2/Task3.java
+++ src/main/java/day2/Task3.java
@@ -1,7 +1,21 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-
+        Scanner scan = new Scanner(System.in);
+        int x = scan.nextInt();
+        int b = scan.nextInt();
+        if (x >= b) {
+            System.out.println("Некорректный ввод");
+        }
+        int a = x + 1;
+        while (a < b) {
+            if (a % 5 == 0 && a % 10 != 0) {
+                System.out.print(a + " ");
+            }
+            a++;
+        }
     }
 }
